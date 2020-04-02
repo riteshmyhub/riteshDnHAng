eb88a370d802e988eda76ca03c37aea27cac663c
ritesh goswami <you@example.com>
2020-04-02 07:56:31 +0530

    add about us page & add cdn animate css

--- src/app/home/aboutus/aboutus.component.ts
+++ src/app/home/aboutus/aboutus.component.ts
@@ -6,7 +6,74 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./aboutus.component.css']
 })
 export class AboutusComponent implements OnInit {
-
+   
+  History:any[]=[
+    { 
+      'slideAttr':'0',
+     'year':'1967',
+     'name':'D&H 1200 T: Nickel base alloy electrode conforming to the AWS classification ENICRFe2'    
+    },
+    { 
+      'slideAttr':'1',
+      'year':'1968',
+      'name':'D&H 1200 T: Nickel base alloy electrode conforming to the AWS classification ENICRFe2' 
+    },
+    { 
+      'slideAttr':'2',
+      'year':'1969',
+      'name':'D&H 1200 T: Nickel base alloy electrode conforming to the AWS classification ENICRFe2' 
+    },
+    { 
+      'slideAttr':'3',
+      'year':'1970',
+      'name':'D&H 1200 T: Nickel base alloy electrode conforming to the AWS classification ENICRFe2' 
+    },
+    { 
+      'slideAttr':'4',
+      'year':'1971',
+      'name':'D&H 1200 T: Nickel base alloy electrode conforming to the AWS classification ENICRFe2' 
+    },
+    { 
+      'slideAttr':'5',
+      'year':'1972',
+      'name':'D&H 1200 T: Nickel base alloy electrode conforming to the AWS classification ENICRFe2' 
+    },
+    { 
+      'slideAttr':'6',
+      'year':'1973',
+      'name':'D&H 1200 T: Nickel base alloy electrode conforming to the AWS classification ENICRFe2' 
+    },
+    { 
+      'slideAttr':'7',
+      'year':'1974',
+      'name':'D&H 1200 T: Nickel base alloy electrode conforming to the AWS classification ENICRFe2' 
+    },
+    { 
+      'slideAttr':'8',
+      'year':'1975',
+      'name':'D&H 1200 T: Nickel base alloy electrode conforming to the AWS classification ENICRFe2' 
+    },
+    { 
+      'slideAttr':'9',
+      'year':'1976',
+      'name':'D&H 1200 T: Nickel base alloy electrode conforming to the AWS classification ENICRFe2' 
+    },
+    { 
+      'slideAttr':'10',
+      'year':'1977',
+      'name':'D&H 1200 T: Nickel base alloy electrode conforming to the AWS classification ENICRFe2' 
+    },
+    { 
+      'slideAttr':'11',
+      'year':'1978',
+      'name':'D&H 1200 T: Nickel base alloy electrode conforming to the AWS classification ENICRFe2' 
+    },
+    { 
+      'slideAttr':'12',
+      'year':'1979',
+      'name':'D&H 1200 T: Nickel base alloy electrode conforming to the AWS classification ENICRFe2' 
+    },
+  ];
   constructor() { }
 
   ngOnInit(): void {
