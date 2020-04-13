d40e848d608181759ac187526242c2facbe9fd3b
ritesh goswami <you@example.com>
2020-04-13 21:53:16 +0530

    work on MQ

--- src/app/home/career/career.component.ts
+++ src/app/home/career/career.component.ts
@@ -1,15 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-career',
-  templateUrl: './career.component.html',
-  styleUrls: ['./career.component.css']
+  selector: "app-career",
+  templateUrl: "./career.component.html",
+  styleUrls: ["./career.component.css"],
 })
 export class CareerComponent implements OnInit {
+  card: any[] = [
+    // card data 1
+    {
+      ImageUrl: "../../../assets/career/c1.png",
+      heading: "Sales & Marketing",
+    },
+    // card data 2
+    {
+      ImageUrl: "../../../assets/career/c2.png",
+      heading: "Production & Planning",
+    },
+    // card data 3
+    {
+      ImageUrl: "../../../assets/career/c3.png",
+      heading: "Research Development",
+    },
+    // card data 4
+    {
+      ImageUrl: "../../../assets/career/c4.png",
+      heading: "Quality Control",
+    },
+    // card data 5
+    {
+      ImageUrl: "../../../assets/career/c5.png",
+      heading: "Logistics & Supply",
+    },
+    // card data 6
+    {
+      ImageUrl: "../../../assets/career/c6.png",
+      heading: "Information Technology",
+    },
+    // card data 7
+    {
+      ImageUrl: "../../../assets/career/c7.png",
+      heading: "Legals & Corporate Affairs",
+    },
+    // card data 8
+    {
+      ImageUrl: "../../../assets/career/c8.png",
+      heading: "Finance & Customer Support",
+    },
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
